c09bf9b456908eacb8e4b5cb32008fcbeded0e42
Wilhelm971 <276351@stud.inn.no>
2025-11-10 15:13:04 +0100

    Player Controller
    Making a camera, and the controllers for moving the camera

---
+++ Source/Group2_Exam/Public/TopDownPlayerController.h
@@ -0,0 +1,68 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "InputActionValue.h"
+#include "TopDownPlayerController.generated.h"
+
+/**
+ * A Player Controller for a top-down tower defence game
+ * Handles camera movement, mouse cursor visibility and later the buying new towers
+ */
+
+
+class UInputMappingContext;
+class UInputAction;
+
+UCLASS()
+class GROUP2_EXAM_API ATopDownPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	ATopDownPlayerController();
+
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+private:
+	// --- CAMERA ---
+	UPROPERTY(EditAnywhere, Category="Camera")
+	float PanSpeed = 2000.0f;
+
+	UPROPERTY(EditAnywhere, Category="Camera")
+	float ZoomSpeed = 5000.0f;
+
+	UPROPERTY(EditAnywhere, Category="Camera")
+	float MinZoom = 1500.0f;
+
+	UPROPERTY(EditAnywhere, Category="Camera")
+	float MaxZoom = 5000.0f;
+
+	// Current zoom target length
+	float TargetArmLength = 2500.0f;
+
+	// Cached reference to the controlled pawn (the top-down camera pawn)
+	APawn* ControlledPawn;
+
+
+
+	
+	// --- INPUT SYSTEM ---
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	UInputMappingContext* InputMapping;
+
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	UInputAction* ZoomAction;
+
+	// --- INPUT HANDLERS ---
+	void HandleMove(const FInputActionValue& Value);
+	void HandleZoom(const FInputActionValue& Value);
+	
+};
