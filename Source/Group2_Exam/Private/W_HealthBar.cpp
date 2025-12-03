e5bfb26d3eeac7a91a27034aef3c36c9f7fbb5d7
Marken2341 <marcusmat16@gmail.com>
2025-12-03 15:02:33 +0100

    Commenting Healthbar

--- Source/Group2_Exam/Private/W_HealthBar.cpp
+++ Source/Group2_Exam/Private/W_HealthBar.cpp
@@ -3,16 +3,18 @@
 
 #include "W_HealthBar.h"
 
+// Sets HealthBar to 100 percent
 void UW_HealthBar::NativeConstruct()
 {
 	Super::NativeConstruct();
-
+	
 	if (ProgressBar)
 	{
 		ProgressBar->SetPercent(1.0f);
 	}
 }
 
+// Sets the progress bar status, and to keep it updated with the assigned objects healthbar
 void UW_HealthBar::SetProgress(float Progress)
 {
 	if (!ProgressBar) return;
--- Source/Group2_Exam/Public/W_HealthBar.h
+++ Source/Group2_Exam/Public/W_HealthBar.h
@@ -16,9 +16,11 @@ class GROUP2_EXAM_API UW_HealthBar : public UUserWidget
 	GENERATED_BODY()
 
 public:
+	// Initates Functions
 	UFUNCTION(BlueprintCallable)
 	void SetProgress(float Progress);
 
+	// Reference to the ProgressBar.h
 	UPROPERTY(meta = (BindWidget))
 	UProgressBar* ProgressBar;
 protected:
